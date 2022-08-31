7656787434b964e5fedf740a5b1e1811d84338ba
DuarteGe <111998883+DuarteGe@users.noreply.github.com>
2022-08-31 11:56:05 -0300

    new file:   index.html 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,33 @@
+var num = document.getElementById('')
+
+function soma(){
+    window.alert("Soma!")
+}
+
+function sub(){
+    window.alert("Subtração!")
+}
+
+function mult(){
+    window.alert("Multiplicação!")
+}
+
+function divi(){
+    window.alert("Divisão!")
+}
+
+function porc(){
+    window.alert("Porcentagem!")
+}
+
+function nume(){
+
+    num.innerText = "7"
+
+    /*switch(){
+        case 0:
+
+    }
+
+    document.getElementById("")*/
+}
